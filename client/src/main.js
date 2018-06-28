838ccc99fb5208d8426b4c580192841b9431501c
Ippei SUZUKI <ippei0605@gmail.com>
2018-06-28 14:55:25 +0900

    Element-UI 組み込み、ESLint 調整

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import Element from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/ja';
+import locale from 'element-ui/lib/locale';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+// configure language
+locale.use(lang);
+
+Vue.use(Element, {size: 'small'});
+Vue.use(Element, {locale});
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
